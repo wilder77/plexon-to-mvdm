fbbde3c40a26d3c4cd726e042b01f71657333454
Mike H <jmhasse01@gmail.com>
2016-03-17 20:34:55 -0400

    streamlined, turned into function

--- Pl2tomvdmspike.m
+++ Pl2tomvdmspike.m
@@ -1,36 +1,27 @@
-%% Creates struct of spike time stamps for pre-sorted channels in mvdm format
-% select file to load
-name = 'Enter Number of files to analyze';
-prompt = {'Number of Files'};
-numlines = 1;
-defaultparams = {'1'};
-params = inputdlg(prompt,name,numlines,defaultparams);
-numRep = str2double(params{1});
+%% Creates struct of spike time stamps for pre-sorted channels in mvdm format from filelist of file paths
+function [Spk] = Pl2tomvdmspike(filelist);
+for r = 1:length(filelist)
+    %%
+    data(r) = PL2GetFileIndex(filelist{r,1});
+    
+    %%
+    %Find channels with sorted units
+    for k = 1:length(data(r).SpikeChannels)
+        spikechan(k,1) = data(r).SpikeChannels{k,1}.NumberOfUnits > 0; %Integrity check: Makes sure spikes are from pre-sorted, optimized units
+    end
+    spikechan = find(spikechan); %creates an index to find channels with sorted units within larger data struct (named "data")
+    for j = 1:length(spikechan)
+        goodchannels(j,1) = data(r).SpikeChannels(spikechan(j,:)); %creates struct of channels with sorted units
+    end
 
-for Reps = 1:numRep
-    [datafile,path] = uigetfile('D:\PlexonData\FerretCGdata'); % on Briggs lab data computer, can be changed to local data repository
-    cd (path);
+    %%
+    %Create data structure containing sorted spikes with MvdM organization
+    Spk = struct([]);
+    Spk(1).type = 'ts';
+    for i = 1:length(goodchannels);
+        Spk(1).t{i} = PL2Ts(filelist{r},goodchannels{i}.Name,1);
+        Spk(1).label{i} = goodchannels{i}.Name;
+        Spk(1).cfg{i} = goodchannels{i};
+    end
     
-        data = PL2GetFileIndex(datafile);
-        
-        %%
-        %Find channels with sorted units
-        for k = 1:length(data.SpikeChannels)
-            spikechan(k,1) = data.SpikeChannels{k,1}.NumberOfUnits > 0; %Integrity check: Makes sure spikes are from pre-sorted, optimized units
-        end
-        spikechan = find(spikechan); %creates an index to find channels with sorted units within larger data struct (named "data")
-        for j = 1:length(spikechan)
-            goodchannels(j,1) = data.SpikeChannels(spikechan(j,:)); %creates struct of channels with sorted units
-        end
-        %%
-        %Create data structure containing sorted spikes with MvdM organization
-        Spk = struct([]);
-        Spk(1).type = 'ts';
-        for i = 1:length(goodchannels);
-            Spk(1).t{i} = PL2Ts(datafile,goodchannels{i}.Name,1);
-            Spk(1).label{i} = datafile;
-            Spk(1).cfg{i} = goodchannels{i};
-        end
-    
-   
 end
